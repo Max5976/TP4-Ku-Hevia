199561338f6c0d44eaac361a1fae9ce392677167
MAXIMO HEVIA <95097703@ort>
2025-05-07 07:59:42 -0300

    Primer acercamiento a la modelización

---
+++ Models/Ahorcado.cs
@@ -0,0 +1,29 @@
+static class ahorcado {
+
+    static List<string> palabras {get; set;}
+
+    static void inicializarVariable() 
+    {
+        palabras.Add("programacion");
+        palabras.Add("java");
+        palabras.Add("python");
+        palabras.Add("csharp");
+        palabras.Add("javascript");
+        palabras.Add("html");
+        palabras.Add("css");
+        palabras.Add("ahorcado");
+        palabras.Add("algoritmo");
+        palabras.Add("computadora");
+        palabras.Add("desarrollador");
+        palabras.Add("tecnologia");
+        palabras.Add("internet");
+        palabras.Add("redes");
+        palabras.Add("software");
+        palabras.Add("hardware");
+        palabras.Add("app");
+        palabras.Add("database");
+        palabras.Add("framework");
+        palabras.Add("algoritmos");
+        palabras.Add("código");
+    }
+}
